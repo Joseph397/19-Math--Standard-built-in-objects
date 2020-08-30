3b13b705c46bab9ee8563a3253d3c9dbc7ca974f
Joseph397 <gramkracker88@gmail.com>
2020-08-29 17:10:33 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Math 6:50:38
+// Standard built-in objects- always available
+
+// const number = 4.96789;
+// const result = Math.floor(number); // floor rounds up no matter what
+
+// const number = 4.1;
+// const result = Math.ceil(number); // celi rounds up no matter what
+
+// const number = 89;
+// const result = Math.sqrt(number); // sqrt gets the square root of a number
+
+// const result = Math.PI; // results in the Value of PI
+// const result = Math.min(4,5,6,7,9); // returns minimum values
+// const result = Math.max(4,5,6,7,9,100,200,1000);
+
+// const result = Math.random(); // returns a random number between 0 and .99
+
+// const result = Math.random() * 10; // returns random number between 0 and 9.99
+
+// const result = Math.floor(Math.random() * 10); // returns random number between 0 and 9 , random passed into floor removes decimals 
+
+// const result = Math.ceil(Math.random() * 10); // returns random between 1 and 10
+
+const result = Math.floor(Math.random() * 10 + 1 ); // Also returns random Between 1 and 10
+
+console.log(result);
